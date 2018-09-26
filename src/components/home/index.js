4130e2738046bc9f45503c3c3da74dfed7e0fce4
David Gurr <kaeyangtheg@users.noreply.github.com>
2018-09-26 05:08:14 -0400

    sky gradient

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -44,7 +44,11 @@ class Home extends React.Component {
   }
 }
 
-const Sky = () => (
+const SunRise = {
+  background: '-webkit-radial-gradient(bottom, circle, rgba(242,248,247,1) 0%,rgba(249,249,28,1) 3%,rgba(247,214,46,1) 8%, rgba(253,50,41,1) 20%,rgba(201,165,132,1) 30%,rgba(115,130,133,1) 51%,rgba(46,97,122,1) 85%,rgba(24,75,106,1) 100%)'
+}
+
+const Sky = ({style}) => (
   <div
     style={{
       zIndex: -1,
@@ -53,14 +57,15 @@ const Sky = () => (
       left: 0,
       width: '100%',
       height: '100%',
-      background: '#70CDFB'
+      background: '#70CDFB',
+      ...style
     }}>
     </div>
 )
 
 export const HomeLogo = ({history}) => (
   <Home>
-    <Sky />
+    <Sky style={SunRise} />
     <FadeContainer style={{width: '50vw', paddingTop: '100px'}}>
       <Logo handleOnClick={() => history.push('/mission')}/>
     </FadeContainer>
